2fc4ab21cbde4ef7170d9c6e7c57a57b2713867c
Artem Valkov <valkov.artyom@gmail.com>
2019-11-18 10:04:31 +0200

    Updates

---
+++ precache-manifest.13be4080be5f7ef18b16bd6dc1f0ca14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62651c6db4a4b218a951db78eb088921",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "d718ba51d2ffa3f1bf53",
+    "url": "/todo-app-react/static/css/main.421fc5c0.chunk.css"
+  },
+  {
+    "revision": "9db49ef97c61804b6199",
+    "url": "/todo-app-react/static/js/2.bedd8747.chunk.js"
+  },
+  {
+    "revision": "d718ba51d2ffa3f1bf53",
+    "url": "/todo-app-react/static/js/main.e4eb686f.chunk.js"
+  },
+  {
+    "revision": "352e9a8e620bb22a21f9",
+    "url": "/todo-app-react/static/js/runtime~main.e12301e2.js"
+  }
+]);
